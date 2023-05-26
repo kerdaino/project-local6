7fff79459a8eb749aa7970c7f0a59364ca498e46
kerdaino <kerdaino3@gmail.com>
2023-05-26 05:18:44 -0700

    first-commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const topicElement = document.querySelector(".topic");
+const countElement = document.querySelector(".count");
+const learning = {};
+
+learning.topic = "JS";
+learning.learningGoals = ["Build JS programs","Mockup Language","JS REACT","GITHUB"];
+learning.category = "Front-End Development";
+learning.topicImportance = "High";
+
+console.log(learning);
+
+learning.topic = "JavaScript";
+learning.learningGoals.splice(1,1);
+
+topicElement.innerText = `I'm learning ${learning.topic}.`;
+topicElement.classList.remove("hide");
+
+countElement.innerText = `I have ${learning.learningGoals.length} learning goals`;
+countElement.classList.remove("hide");
